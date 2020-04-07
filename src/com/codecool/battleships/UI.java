4cb4305655dbe43a38062bf88e181ff236fab596
Aldonka <aldonapawlak2@gmail.com>
2020-04-07 13:43:28 +0200

    add attributes

--- src/com/codecool/battleships/UI.java
+++ src/com/codecool/battleships/UI.java
@@ -1,4 +1,14 @@
 package com.codecool.battleships;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class UI {
+    private Scanner input = new Scanner(System.in);
+    private Boolean isRunning = true;
+    private List<String> options = Arrays.asList("SinglePlayer", "Multiplayer", "Simulation", "Exit");
+    private List<String> difficulties = Arrays.asList("Easy", "Medium", "Hard");
+    // private Player player = new Player(null);
 }
