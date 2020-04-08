e04ccabb60598ae7516e5e09c3308f249df5320d
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-04-08 13:53:35 +0200

    Change ship constructor and parameters

--- src/com/codecool/battleships/Ship.java
+++ src/com/codecool/battleships/Ship.java
@@ -5,74 +5,78 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class Ship {
-    int length;
-    Boolean isHorizontal;
-    Boolean isSunk;
-    private int x;
-    private int y;
-    List<Square> shipSquares = new ArrayList<>();
-    private String shipSign = "S";
+    private int coordinateX;
+    private int coordinateY;
+    private int shipLength;
+    private String orientation;
+    private String shipType;
+    private ArrayList<Square> shipSquaresList;
 
-    public Ship(int length, Boolean isHorizontal, int x, int y, Square[][] board) {
-        this.length = length;
-        this.isHorizontal = isHorizontal;
-        this.isSunk = false;
-        setShip(length, x, y, board);    
-    }  
-    public void setXofShip(int x){
-        this.x = x;
-    }
-    public void setYofShip(int y){
-        this.y = y;
-    }
-    public int getXofShip(){
-        return x;
-    }
-    public int getYofShip(){
-        return y;
-    } 
-    public void sunk() {
-        this.isSunk = true;
-    }
-
-  public Map<String, Integer> getShipsLength() {
-        Map<String, Integer> shipsLength = new HashMap<>();
-        shipsLength.put("Carrier", 5);
-        shipsLength.put("Battleship", 4);
-        shipsLength.put("Cruiser", 3);
-        shipsLength.put("Submarine", 3);
-        shipsLength.put("Destroyer", 2);
-
-        return shipsLength;
-    }
-
-
-    private void setShip(int length, int x, int y, Square [][] board) {
-        Square position;
-
-        for (int i = 0; i < length; i++) {
-
-            if (this.isHorizontal) {
-                position = new Square(x + i, y);
-                position.setSquareStatus(shipSign);
-                board[y][x + i] = position;
-            } else {
-                position = new Square(x, y + i);
-                position.setSquareStatus(shipSign);
-                board[y + i][x] = position;
-            }
-            this.shipSquares.add(position);
-        }
-    }
-
-    public Boolean checkIfSunked() {
-        for (Square position : shipSquares) {
-            if (!position.getSquareStatus().equals("x")) {
-                return false;
-            }
-        }
-        return true;
+    Ship(int coordinateX, int coordinateY, int shipLength, String orientation, String shipType) {
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+        this.shipLength = shipLength;
+        this.orientation = orientation.toLowerCase();
+        this.shipType = shipType;
+        this.shipSquaresList = new ArrayList<>();
     }
 }
+// TO BE CHANGED:
+
+//    public void setXofShip(int x){
+//        this.x = x;
+//    }
+//    public void setYofShip(int y){
+//        this.y = y;
+//    }
+//    public int getXofShip(){
+//        return x;
+//    }
+//    public int getYofShip(){
+//        return y;
+//    }
+//    public void sunk() {
+//        this.isSunk = true;
+//    }
+//
+//  public Map<String, Integer> getShipsLength() {
+//        Map<String, Integer> shipsLength = new HashMap<>();
+//        shipsLength.put("Carrier", 5);
+//        shipsLength.put("Battleship", 4);
+//        shipsLength.put("Cruiser", 3);
+//        shipsLength.put("Submarine", 3);
+//        shipsLength.put("Destroyer", 2);
+//
+//        return shipsLength;
+//    }
+//
+//
+//    private void setShip(int length, int x, int y, Square [][] board) {
+//        Square position;
+//
+//        for (int i = 0; i < length; i++) {
+//
+//            if (this.isHorizontal) {
+//                position = new Square(x + i, y);
+//                position.setSquareStatus(shipSign);
+//                board[y][x + i] = position;
+//            } else {
+//                position = new Square(x, y + i);
+//                position.setSquareStatus(shipSign);
+//                board[y + i][x] = position;
+//            }
+//            this.shipSquares.add(position);
+//        }
+//    }
+//
+//    public Boolean checkIfSunked() {
+//        for (Square position : shipSquares) {
+//            if (!position.getSquareStatus().equals("x")) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
